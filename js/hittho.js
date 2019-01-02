dabdfb1e5537e8d1463afadba6923be5b98c8110
gl <gl@localhost>
2019-01-02 22:19:08 +0700

    add pop up

--- js/hittho.js
+++ js/hittho.js
@@ -1,6 +1,12 @@
 var orb = document.getElementById("orb");
 var start = document.getElementById("start");
+var ok = document.getElementById("ok");
+var pop = document.getElementById("pop");
 
 start.onclick = function() {
   orb.classList.add("orb_1");
+};
+
+ok.onclick = function() {
+  pop.style.display = "none";
 };
